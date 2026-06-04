26ff02806ad149eb1c96ce103639fef39ac44c37
Sumit Kumar Maurya <sumitmaurya005.dev@gmail.com>
2026-06-04 18:21:08 +0530

    Add dynamic export to page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
 import CTA from "@/components/CTA";
